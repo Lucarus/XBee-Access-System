a6d2ac6b0d0f307eb24fe9746ada08b7a94a16b1
Lukas <lukas_sturm@yahoo.de>
2017-05-01 15:06:48 +0200

    new Data

--- startWin.bat
+++ startWin.bat
@@ -1,5 +1,5 @@
 @echo off
 
-start cmd.exe /k "python accessAPI.py"
-start cmd.exe /k "python accessWebsocket.py"
+start cmd.exe /k "python accessAPI.py 192.168.2.42"
+start cmd.exe /k "python accessWebsocket.py 192.168.2.42"
 start cmd.exe /k "python XBeeReceiver.py COM8"
